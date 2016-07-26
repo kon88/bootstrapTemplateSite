5eefce3df6bb0b382b71f2e01ec103faa98eb12f
Kon Kim <kon.kim@outlook.com>
2016-07-25 20:37:21 -0400

    Initial

---
+++ js/myscript.js
@@ -0,0 +1,120 @@
+$(function() {
+
+  "use strict";
+
+  var topoffset = 50; //variable for menu height
+  var slideqty = $('#featured .item').length;
+  var wheight = $(window).height(); //get the height of the window
+  var randSlide = Math.floor(Math.random()*slideqty);
+
+  $('#featured .item').eq(randSlide).addClass('active');
+
+
+  $('.fullheight').css('height', wheight); //set to window tallness  
+
+
+  //replace IMG inside carousels with a background image
+  $('#featured .item img').each(function() {
+    var imgSrc = $(this).attr('src');
+    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+    $(this).remove();
+  });
+
+  //adjust height of .fullheight elements on window resize
+  $(window).resize(function() {
+    wheight = $(window).height(); //get the height of the window
+    $('.fullheight').css('height', wheight); //set to window tallness  
+  });
+
+
+
+  //Activate Scrollspy
+  $('body').scrollspy({
+    target: 'header .navbar',
+    offset: topoffset
+  });
+
+  // add inbody class
+  var hash = $(this).find('li.active a').attr('href');
+  if(hash !== '#featured') {
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+
+
+  // Add an inbody class to nav when scrollspy event fires
+  $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+      $('header nav').addClass('inbody');
+    } else {
+      $('header nav').removeClass('inbody');
+    }
+  });
+
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+  //Automatically generate carousel indicators
+  for (var i=0; i < slideqty; i++) {
+    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
+    if (i === randSlide) {
+      insertText += ' class="active" ';
+    }
+    insertText += '></li>';
+    $('#featured ol').append(insertText);
+  }
+
+  $('.carousel').carousel({
+    pause: false
+  });
+  
+  $('#myModal').on('shown.bs.modal', (function() {
+  var mapIsAdded = false;
+
+  return function() {
+    if (!mapIsAdded) {
+      $('.modal-body').html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3018.923378135842!2d-73.92836318511521!3d40.82964663834773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2f42c5ae76971%3A0x4940c3d9559a1e08!2sYankee+Stadium!5e0!3m2!1sen!2sus!4v1446598392586" width="100%" height="400" frameborder="0" style="border:0"></iframe>');
+
+      mapIsAdded = true;
+    }    
+  };
+})());
+
+$(function() {
+//twitter bootstrap script
+ $(".button #submit").click(function(){
+         $.ajax({
+     type: "POST",
+ url: "process.php",
+ data: $('form.contact').serialize(),
+         success: function(msg){
+                 $("#thanks").html(msg)
+        $("#form-content").modal('hide'); 
+         },
+ error: function(){
+ alert("failure");
+ }
+       });
+ });
+});
+
+  
+  
+
+});
